4f7c3c73baa3f2fb216d665c28c1bd599620bda7
ilagon <lorenzyadrian@gmail.com>
2022-05-26 20:48:29 +0800

    Created Login View

---
+++ android/app/src/main/kotlin/com/example/famapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.famapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/color_palette.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color.fromRGBO(254, 246, 228, 1);
+const primaryColor = Color.fromRGBO(243, 210, 193, 1);
+const secondaryColor = Color.fromRGBO(245, 130, 174, 1);
+const textColor = Color.fromRGBO(0, 24, 88, 1);
